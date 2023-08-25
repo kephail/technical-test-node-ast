bba72630076a70bef79a0cdaa6fdf8df3b1e6614
Peter Jones <hi@peterjones.io>
2023-08-25 09:42:14 +0100

    Ran prettier before getting started

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const { generate } = require('astring');
-const { parseScript } = require('meriyah');
-const fs = require('fs');
-const fileEncoding = 'utf8';
+const { generate } = require("astring");
+const { parseScript } = require("meriyah");
+const fs = require("fs");
+const fileEncoding = "utf8";
 
-console.log('Hello from Ably!');
-console.log(`${generate ? '✔' : '✖'} astring.generate`);
-console.log(`${parseScript ? '✔' : '✖'} meriyah.parseScript`);
+console.log("Hello from Ably!");
+console.log(`${generate ? "✔" : "✖"} astring.generate`);
+console.log(`${parseScript ? "✔" : "✖"} meriyah.parseScript`);
 
 // read from file (you don't need to modify this line)
-const input = fs.readFileSync('input.js', fileEncoding);
-
+const input = fs.readFileSync("input.js", fileEncoding);
 
 // TODO replace this line with your own code
 const output = input;
 
-
 // write to file (you don't need to modify this line)
-fs.writeFileSync('output.js', output, fileEncoding);
+fs.writeFileSync("output.js", output, fileEncoding);
