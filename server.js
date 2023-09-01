5d9af226353b2dc7cad26df7570b6f72af873c1a
Peter Jones <hi@peterjones.io>
2023-09-01 15:10:44 +0100

    Logging for part 2

--- server.js
+++ server.js
@@ -7,22 +7,26 @@ console.log("Hello from Ably!");
 console.log(`${generate ? "✔" : "✖"} astring.generate`);
 console.log(`${parseScript ? "✔" : "✖"} meriyah.parseScript`);
 
-const transformVarToConst = (node) => {
+const transformAST = (node) => {
   if (node.type === "VariableDeclaration" && node.kind === "var") {
     return Object.assign({}, node, {
       kind: "const",
-      declarations: node.declarations.map(transformVarToConst),
+      declarations: node.declarations.map(transformAST),
     });
   }
 
+  if (node.type === "FunctionDeclaration" && node.async) {
+    console.dir(node, { depth: null });
+  }
+
   if (Array.isArray(node)) {
-    return node.map(transformVarToConst);
+    return node.map(transformAST);
   }
 
   if (typeof node === "object") {
     const newNode = {};
     for (const key in node) {
-      newNode[key] = transformVarToConst(node[key]);
+      newNode[key] = transformAST(node[key]);
     }
     return newNode;
   }
@@ -32,10 +36,17 @@ const transformVarToConst = (node) => {
 
 // read from file (you don't need to modify this line)
 const input = fs.readFileSync("input.js", fileEncoding);
+const input2 = fs.readFileSync("input2.js", fileEncoding);
 
 const ast = parseScript(input);
-const transformedAst = transformVarToConst(ast);
+const ast2 = parseScript(input2);
+
+// console.dir(ast2, { depth: null });
+
+const transformedAst = transformAST(ast);
+const transformedAst2 = transformAST(ast2);
 const output = generate(transformedAst);
 
 // write to file (you don't need to modify this line)
 fs.writeFileSync("output.js", output, fileEncoding);
+fs.writeFileSync("output2.js", output, fileEncoding);
