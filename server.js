40e2941cc0993739a8f28b26cd909a578ec410ce
Peter Jones <hi@peterjones.io>
2023-09-01 13:54:25 +0100

    task 1

--- server.js
+++ server.js
@@ -7,14 +7,35 @@ console.log("Hello from Ably!");
 console.log(`${generate ? "✔" : "✖"} astring.generate`);
 console.log(`${parseScript ? "✔" : "✖"} meriyah.parseScript`);
 
+const transformVarToConst = (node) => {
+  if (node.type === "VariableDeclaration" && node.kind === "var") {
+    return Object.assign({}, node, {
+      kind: "const",
+      declarations: node.declarations.map(transformVarToConst),
+    });
+  }
+
+  if (Array.isArray(node)) {
+    return node.map(transformVarToConst);
+  }
+
+  if (typeof node === "object") {
+    const newNode = {};
+    for (const key in node) {
+      newNode[key] = transformVarToConst(node[key]);
+    }
+    return newNode;
+  }
+
+  return node;
+};
+
 // read from file (you don't need to modify this line)
 const input = fs.readFileSync("input.js", fileEncoding);
 
-// TODO replace this line with your own code
-const output = input;
-
 const ast = parseScript(input);
-console.log(ast); // Checking what parseScript returns
+const transformedAst = transformVarToConst(ast);
+const output = generate(transformedAst);
 
 // write to file (you don't need to modify this line)
 fs.writeFileSync("output.js", output, fileEncoding);
