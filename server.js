acda4e322c63f0a0da01599fddd9a903a21e42da
Peter Jones <hi@peterjones.io>
2023-09-04 09:58:45 +0100

    WIP on part 2
    - trying to use astring generator to convert async await to promise based code

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const { generate } = require("astring");
+const astring = require("astring");
 const { parseScript } = require("meriyah");
 const fs = require("fs");
 const fileEncoding = "utf8";
 
 console.log("Hello from Ably!");
-console.log(`${generate ? "✔" : "✖"} astring.generate`);
+console.log(`${astring.generate ? "✔" : "✖"} astring.generate`);
 console.log(`${parseScript ? "✔" : "✖"} meriyah.parseScript`);
 
 const transformAST = (node) => {
+  // replace var with const
   if (node.type === "VariableDeclaration" && node.kind === "var") {
     return Object.assign({}, node, {
       kind: "const",
@@ -15,8 +16,11 @@ const transformAST = (node) => {
     });
   }
 
+  // Remove async keyword from async functions
   if (node.type === "FunctionDeclaration" && node.async) {
-    console.dir(node, { depth: null });
+    return Object.assign({}, node, {
+      async: false,
+    });
   }
 
   if (Array.isArray(node)) {
@@ -34,6 +38,20 @@ const transformAST = (node) => {
   return node;
 };
 
+//Custom generator for astring to convert await to promise
+const customGenerator = Object.assign({}, astring.GENERATOR, {
+  AwaitExpression: function (node, state) {
+    this[node.argument.type](node.argument, state);
+    state.write(".then((result) => { ");
+    state.write("console.log(result);");
+    state.write(" })");
+  },
+});
+
+const addNewlineAfterImport = (code) => {
+  return code.replace(/(const .+ = require\(.+\);)/, "$1\n");
+};
+
 // read from file (you don't need to modify this line)
 const input = fs.readFileSync("input.js", fileEncoding);
 const input2 = fs.readFileSync("input2.js", fileEncoding);
@@ -41,12 +59,16 @@ const input2 = fs.readFileSync("input2.js", fileEncoding);
 const ast = parseScript(input);
 const ast2 = parseScript(input2);
 
-// console.dir(ast2, { depth: null });
-
 const transformedAst = transformAST(ast);
 const transformedAst2 = transformAST(ast2);
-const output = generate(transformedAst);
+
+const output = astring.generate(transformedAst);
+const output2 = astring
+  .generate(transformedAst2, { generator: customGenerator })
+  .replace(/"/g, "'");
+
+const modifiedOutput2 = addNewlineAfterImport(output2);
 
 // write to file (you don't need to modify this line)
 fs.writeFileSync("output.js", output, fileEncoding);
-fs.writeFileSync("output2.js", output, fileEncoding);
+fs.writeFileSync("output2.js", modifiedOutput2, fileEncoding);
